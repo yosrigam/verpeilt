41be0f2e59bee0a2ef554a5960d5689d10a718b4
Yosri Gam <yosri.gam@hyperlink.eu>
2023-03-11 16:58:52 +0100

    use url instead of asset

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,7 +9,7 @@
     </head>
     <body class="antialiased">
         <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-        <img src="{{asset('images/logo-no-background.svg',true)}}" alt="Verpeilt-logo" class="rounded-lg" />
+        <img src="{{ url('storage/images/logo-no-background.svg') }}" alt="Verpeilt-logo" class="rounded-lg" />
         </div>
     </body>
 </html>
